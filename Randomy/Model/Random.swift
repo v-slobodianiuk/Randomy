4be36a88a387b8da8e6bb4efcb7d5d980f373df2
Vadym <dev.slobodianiuk@gmail.com>
2020-03-31 00:13:46 +0300

    Xcode bug

--- Randomy/Model/Random.swift
+++ Randomy/Model/Random.swift
@@ -2,8 +2,28 @@
 //  Random.swift
 //  Randomy
 //
-//  Created by Vadym on 30.03.2020.
+//  Created by Vadym on 25.03.2020.
 //  Copyright © 2020 Vadym Slobodianiuk. All rights reserved.
 //
 
 import Foundation
+
+struct Random {
+    static let shared = Random()
+    
+    private init() {}
+    
+    // MARK: Method for randomize elements using Timer
+    func getNewValue(repeats: Int, timeInterval: Double, completion: @escaping () -> Void) {
+        
+        var timerTime = 0.0
+        
+        // MARK: For in loop with timer for animate values
+        for _ in 1...repeats {
+            Timer.scheduledTimer(withTimeInterval: timerTime, repeats: false) {_ in
+                completion()
+            }
+            timerTime += timeInterval
+        }
+    }
+}

8d22d5adac0f13989529cfe70eb29876b30ba53e
Vadym <dev.slobodianiuk@gmail.com>
2020-03-31 19:48:09 +0300

    GradientViewTests  setUp(), func tearDown()

--- RandomyTests/ViewTests/GradientViewTests.swift
+++ RandomyTests/ViewTests/GradientViewTests.swift
@@ -15,27 +15,21 @@ class GradientViewTests: XCTestCase {
     
     var testGradient: Gradient?
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
         
         testGradient = Gradient.shared
         testView = UIView()
         testView?.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         testGradient?.initGradient(for: testView!)
-        
-        
-        
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
         
         testGradient = nil
         testView = nil
     }
     
     func testSublayer() {
-        
         XCTAssertEqual(testView!.layer.sublayers![0].name, "Gradient")
     }
 }
